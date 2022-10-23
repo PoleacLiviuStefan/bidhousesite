295a7fc81115915c276ecdf28ae859ba78d04e01
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2022-10-23 23:12:22 +0300

    responsived

--- src/Components/WhoAreWe/WhoAreWe.jsx
+++ src/Components/WhoAreWe/WhoAreWe.jsx
@@ -4,15 +4,15 @@ const WhoAreWe = () => {
   return (
 
     <div name="WhoAreWe" className=" bg-black h-[70rem] w-full">
-      <div className="absolute  z-40 bg-[url('/public/phase1.jpeg')]  bg-cover w-[300px] h-[306px] xl:w-[721px] xl:h-[733px]" />
-      <div className="absolute bg-[url('/public/bg2.png')]  top-[80px] w-[95%] h-[934px]  left-0 bg-cover">
-        <div className="absolute right-[-25rem] z-40 bg-opacity-70 top-[-25rem] bg-[url('/public/glowsecond.png')] bg-cover w-[1304px] h-[1282px]" />
-        <div className="absolute z-40 right-[55px] xl:right-[40px] top-[350px] xl:top-[130px] ">
-          <h2 className=" font-[600] text-[30px] 2xl:text-[76px]  xl:leading-[112px] text-white  bg-clip-text bg-gradient-to-r from-[#9945FF] to-[#17DD8A] ">
+      <div className="absolute rounded-[20px] z-40 bg-[url('/public/phase1.jpeg')]   bg-cover w-[300px] h-[306px] xl:w-[600px] xl:h-[610px] 2xl:w-[721px] 2xl:h-[733px]" />
+      <div className="absolute bg-[url('/public/bg2.png')]  top-[80px] w-[95%] h-[1000px] xl:h-[934px]  left-0 bg-cover">
+        <div className="absolute right-[-25rem] z-30 bg-opacity-70 top-[-25rem] bg-[url('/public/glowsecond.png')] bg-cover w-[1304px] h-[1282px]" />
+        <div className="absolute  z-40 right-[25px] xl:right-[40px] top-[300px] xl:top-[130px] ">
+          <h2 className="relative font-[600]  text-[30px]  leading-[40px] xl:leading-[72px] 2xl:xl:leading-[112px] xl:text-[52px] 2xl:text-[76px]   text-white  bg-clip-text bg-gradient-to-r from-[#9945FF] to-[#17DD8A] ">
             THE BENEFITS OF <br />
             HOLDING <span className="text-transparent">OUR NFT</span>
           </h2>
-          <p className="absolute xl:w-[500px] leading-[30px] 2xl:w-[700px]  top-[140px] xl:top-[270px] font-[300] xl:text-[16px] 2xl:text-[20px] leading-[24px] flex align-center left-0 text-white">
+          <p className="absolute top-[100px]  w-[300px] xl:w-[450px] 2xl:w-[600px] leading-[30px]  xl:top-[180px] 2xl:top-[270px] font-[300] xl:text-[16px] xl:text-[16px] 2xl:text-[20px] leading-[24px] flex align-center left-0 text-white">
           The apartments to be purchased under the project will be offered for rent for three periods of time depending on demand and area: long, medium and short term rental.<br />
 
 Long-term, apartments will be rented to families for a minimum of 1-2 years to ensure financial security for investors and NFT buyers.<br/>
