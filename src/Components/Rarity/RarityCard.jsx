6e7ed06b4a3714207579a4d4ee24f90c665d8508
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2022-10-27 19:32:14 +0300

    saf

--- src/Components/Rarity/RarityCard.jsx
+++ src/Components/Rarity/RarityCard.jsx
@@ -10,7 +10,7 @@ const RarityCard = ({keyimg,title,color,benefit}) => {
             setExpand(false);
             }
   return (
-    <div onMouseLeave={handleExpandBack} onMouseEnter={handleExpand} className={`relative ml-6 mt-[14.35rem] xl:mt-0 rounded-[20px] backdrop-blur-[8px] flex flex items-center justify-center w-[160px] xl:w-[180px] h-[300px] xl:h-[424px] `}>
+    <div onMouseLeave={handleExpandBack} onMouseEnter={handleExpand} className={`relative ml-6 mt-[15rem] xl:mt-0 rounded-[20px] backdrop-blur-[8px] flex flex items-center justify-center w-[160px] xl:w-[180px] h-[300px] xl:h-[424px] `}>
       <Cardtxt color={color} discount={benefit}/>
       
         <div className={`relative  rounded-[20px] backdrop-blur-[8px] flex flex-col items-center w-[180px] ${expand ?'h-[424px] py-10':'h-[344px]'} ${color} bg-opacity-10`}>
